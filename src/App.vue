f43e73930c767c381cb911ba4330934b435ffd23
liangjunke <liangjunke198909@outlook.com>
2025-09-26 16:26:45 +0800

    20250926

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
+<template>
+  <div>
+    <h1>LJK_学习Cesium</h1>
+  </div>
+
+  <div id="cesium-toolbar">
+    toolbar
+  </div>
+  <div id="cesium-viewer">
+
+  </div>
+
+</template>
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import * as Cesium from 'cesium'
+import * as Cesium from 'cesium';
+import {onMounted} from "vue";
+
+onMounted(() => {
+  const myToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyZDY5MDU2ZS1mZTdlLTQzMmUtOGVlNC05NWM0ZDQ3MTUwNmIiLCJpZCI6MzQ0NzIxLCJpYXQiOjE3NTg4NTY4MDN9.TdrDuDBUqo2ZGzQ-sDUIJvBu8KqS5KSO6nx1s88OkQQ';
+  Cesium.Ion.defaultAccessToken = myToken;
+  const esri = new Cesium.ArcGisMapServerImageryProvider({
+    url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer',
+    enablePickFeatures: false,
+  });
+  const viewer = new Cesium.Viewer('cesium-viewer', {
+    imageryProvider: esri,//默认是谷歌的图层
+    terrainProvider: Cesium.createWorldTerrain(
+        {
+          requestWaterMask: true,//水面特效
+        }
+    )//地形图层
+  });
+})
+
 console.log(Cesium)
 </script>
 
-<template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+#cesium-viewer {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
 }
 </style>
